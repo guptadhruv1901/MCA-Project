3a1701a2489b108c7ac509b34833e400826a9cec
guptadhruv1901 <guptadhruv1901@gmail.com>
2025-04-29 00:40:10 +0530

    update

--- FrontEnd/src/Components/Footer/Footer.jsx
+++ FrontEnd/src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <>
     <div className="footer">
-      <div className="logoShow">Wander<span>Whiz</span></div>
+      <div className="logoShow">WANDER <span>Whiz</span></div>
       <div className="footer-section">
         <div className='first'>This is a paragraph where you can include any information you’d like. It’s an opportunity to tell a story about the company, describe a special service it offers, or highlight a particular feature that sets it apart from competitors.</div>
         <div className='second'>
