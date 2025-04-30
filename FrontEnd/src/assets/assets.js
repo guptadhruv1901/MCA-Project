a59dbcadaae09f49108962d04cad71693cbfc9a5
guptadhruv1901 <guptadhruv1901@gmail.com>
2025-04-30 21:41:22 +0530

    update

--- FrontEnd/src/assets/assets.js
+++ FrontEnd/src/assets/assets.js
@@ -1,6 +1,18 @@
 import whiteTree from './HomePage/whiteTree.jpg';
 import calmWaves from './HomePage/calmWaves.mp4';
+import aiPoster from './HomePage/aiPoster.png';
+import mapView from './HomePage/mapView.webp';
+import navigation from './HomePage/navigationPng.png';
+import mapVideo from './HomePage/mapVideo.mp4';
+import weatherPoster from './HomePage/weatherPoster.jpg';
+import weather from './HomePage/weather.png';
 export const assets = {
     whiteTree,
-    calmWaves
+    calmWaves,
+    aiPoster,
+    mapView,
+    navigation,
+    mapVideo,
+    weatherPoster,
+    weather
 }
