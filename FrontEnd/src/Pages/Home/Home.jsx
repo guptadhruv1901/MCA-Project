f34c294e51854591711c375671f41fe5dc0192ac
saumyaverma11 <saumyaverma11052005@gmail.com>
2025-05-03 12:13:36 +0530

    add

--- FrontEnd/src/Pages/Home/Home.jsx
+++ FrontEnd/src/Pages/Home/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
           <h1>ABOUT</h1>
           <div className='upper-content'>
             <div>
-            <video src={assets.calmWaves} autoPlay loop muted></video>
+            <video src={assets.mapVideo} autoPlay loop muted></video>
             </div>              
             <div className="content">Our platform combines AI, live weather, maps, and real-time local insights to help you craft the perfect trip. Whether you're a solo traveler, a family planner, or an adventure junkie — we’ve got your back, one itinerary at a time.</div>
           </div>
@@ -41,7 +41,7 @@ const Home = () => {
         <span><img src={assets.Weaather} alt =""/></span>
         <h1 className='h'>Dream  Plan  Discover</h1>
         <h1 className='center'>Where Every Trip Becomes an Experience.</h1>
-        <span><img src={assets.map} alt =""/></span>
+        <span><img src={assets.weather} alt =""/></span>
       </div>
       <Footer />
     </>
